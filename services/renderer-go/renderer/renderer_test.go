2460d6cb036de6ee0b4aa07b48a299fcccc3fe12
5ebec <hash.a5ebec@gmail.com>
2021-08-18 15:58:47 +0900

    testを変更
    必須課題である
    - 見出し記法
    - リスト記法
    - リンク記法
    に対応させた

--- services/renderer-go/renderer/renderer_test.go
+++ services/renderer-go/renderer/renderer_test.go
@@ -7,9 +7,45 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_Render(t *testing.T) {
+func Test_Render_heading(t *testing.T) {
+	src := `# 見出し1
+## 見出し2
+### 見出し3
+#### 見出し4
+##### 見出し5
+###### 見出し6`
+	html, err := Render(context.Background(), src)
+	assert.NoError(t, err)
+	assert.Equal(t, `<h1>見出し1</h1>
+<h2>見出し2</h2>
+<h3>見出し3</h3>
+<h4>見出し4</h4>
+<h5>見出し5</h5>
+<h6>見出し6</h6>
+`, html)
+}
+
+func Test_Render_list(t *testing.T) {
+	src := ` - foo
+ - bar
+   - baz`
+	html, err := Render(context.Background(), src)
+	assert.NoError(t, err)
+	assert.Equal(t, `<ul>
+<li>foo</li>
+<li>bar
+<ul>
+<li>baz</li>
+</ul>
+</li>
+</ul>
+`, html)
+}
+
+func Test_Render_link(t *testing.T) {
 	src := `foo https://google.com/ bar`
 	html, err := Render(context.Background(), src)
 	assert.NoError(t, err)
-	assert.Equal(t, `foo <a href="https://google.com/">https://google.com/</a> bar`, html)
+	assert.Equal(t, `<p>foo <a href="https://google.com/">https://google.com/</a> bar</p>
+`, html)
 }
